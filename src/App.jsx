6ba8dc3b2ef821b91999a6920bad7f6b536009ed
rohan850683 <rohaan2611@gmail.com>
2025-07-28 08:06:07 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function App() {
 <div className="section-container">
   <h1 className="section-title">Maruti Suzuki</h1>
   <div className="car-section">
-  <Cart Image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq0ULs5E-JppjB_CuPgkaM79cbhz5yO-VYHg&s" Card="Swift" Some="The Maruti Suzuki Swift is a popular compact hatchback known for its sporty design..." Go="Buy Now" url="/Model" />
+  <Cart Image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq0ULs5E-JppjB_CuPgkaM79cbhz5yO-VYHg&s" Card="Swift" Some="The Maruti Suzuki Swift is a popular compact hatchback known for its sporty design..." Go="Buy Now" url="http://localhost:5173/model" />
     <Cart Image="https://img.autocarindia.com/ExtraImages/20241218020740_20190131081704_2019_Maruti_Wagon_R_front_1.jpg" Card="Wagnor" Some="This perfect combination of space, fuel efficiency, reliability..." Go="Buy Now" url="http://localhost:5173/wagnor" />
     <Cart Image="https://imgd-ct.aeplcdn.com/664x415/n/xbceora_1420353.jpg?q=80" Card="Alto" Some="The Maruti Suzuki Alto K10 refers to the model generations..." Go="Buy Now" url="http://localhost:5173/alto" />
     <Cart Image="https://imgcdn.sayaratbay.com/large/gallery/exterior/39/1760/suzuki-baleno-front-angle-low-view-830235.jpg" Card="Baleno" Some="The Maruti Suzuki Baleno is a premium hatchback available in India..." Go="Buy Now" url="http://localhost:5173/baleno" />
@@ -139,12 +139,12 @@ function App() {
 <div className="section-container">
   <h1 className="section-title">Toyota</h1>
   <div className="car-section">
-    <Cart Image="https://stimg.cardekho.com/images/carexteriorimages/930x620/Toyota/Fortuner-Legender/10229/1749726924621/front-left-side-47.jpg" Card="Fortuner" Some="The Mahindra XUV700 is a popular SUV..." Go="Buy Now" url="http://localhost:5173/xuv700" />
-    <Cart Image="https://www.tyremarket.com/tyremantra/wp-content/uploads/2020/04/toyota-glanza.jpg" Card="Glanza" Some="The Mahindra Thar is a rugged and powerful SUV..." Go="Buy Now" url="http://localhost:5173/thar" />
-    <Cart Image="https://imagecdnsa.zigwheels.ae/large/gallery/exterior/40/1867/toyota-innova-hycross-front-angle-low-view-446020.jpg" Card="Innova" Some="The Mahindra Scorpio Classic is a rugged and powerful SUV..." Go="Buy Now" url="http://localhost:5173/scorpio" />
-    <Cart Image="https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/64725b6d365a8f001d7f50a1.jpg" Card="Supra" Some="The Mahindra Bolero is a popular utility vehicle in India..." Go="Buy Now" url="http://localhost:5173/bolero" />
-    <Cart Image="https://media.ed.edmunds-media.com/toyota/corolla-cross/2025/oem/2025_toyota_corolla-cross_4dr-suv_l_fq_oem_1_1280.jpg" Card="Corolla-Cross" Some="The Mahindra BE 6 is an upcoming electric SUV..." Go="Buy Now" url="http://localhost:5173/be-6" />
-    <Cart Image="https://stimg.cardekho.com/images/carexteriorimages/930x620/Toyota/Hilux/10924/1691990326111/front-left-side-47.jpg" Card="Hilux" Some="The Mahindra Alturas G4 is a large, 7-seater SUV..." Go="Buy Now" url="http://localhost:5173/alturas" />
+    <Cart Image="https://stimg.cardekho.com/images/carexteriorimages/930x620/Toyota/Fortuner-Legender/10229/1749726924621/front-left-side-47.jpg" Card="Fortuner" Some="The Fortuner is a popular Car.Its is powerful." Go="Buy Now" url="http://localhost:5173/xuv700" />
+    <Cart Image="https://www.tyremarket.com/tyremantra/wp-content/uploads/2020/04/toyota-glanza.jpg" Card="Glanza" Some="The Glanza is a rugged and powerful Car. Its is under budget car" Go="Buy Now" url="http://localhost:5173/thar" />
+    <Cart Image="https://imagecdnsa.zigwheels.ae/large/gallery/exterior/40/1867/toyota-innova-hycross-front-angle-low-view-446020.jpg" Card="Innova" Some="The Innova Classic is a rugged and powerful SUV. Large in Space" Go="Buy Now" url="http://localhost:5173/scorpio" />
+    <Cart Image="https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/64725b6d365a8f001d7f50a1.jpg" Card="Supra" Some="The Supra is a popular utility vehicle in India.Most famous Vehicle in India" Go="Buy Now" url="http://localhost:5173/bolero" />
+    <Cart Image="https://media.ed.edmunds-media.com/toyota/corolla-cross/2025/oem/2025_toyota_corolla-cross_4dr-suv_l_fq_oem_1_1280.jpg" Card="Corolla-Cross" Some="The Corolla-Cross is an upcoming powerful SUV. This is powerful" Go="Buy Now" url="http://localhost:5173/be-6" />
+    <Cart Image="https://stimg.cardekho.com/images/carexteriorimages/930x620/Toyota/Hilux/10924/1691990326111/front-left-side-47.jpg" Card="Hilux" Some="The Hilux 4x4 is a large, 7-seate SUV" Go="Buy Now" url="http://localhost:5173/alturas" />
   </div>
 </div>
 
@@ -157,7 +157,7 @@ function App() {
     <Cart Image="https://carindia.in/wp-content/uploads/2021/07/P90391325_highRes_the-new-bmw-m5-compe-798x532.jpg" Card="M5" Some="The BMW M5 is a high-performance version of the BMW 5 Series sedan..." Go="Buy Now" url="http://localhost:5173/m5" />
     <Cart Image="https://hips.hearstapps.com/hmg-prod/images/2025-bmw-m8-competition-coupe-101-663a3bf57a59c.jpg?crop=0.771xw:0.291xh;0.189xw,0.542xh&resize=2048:*" Card="M8" Some="The BMW 8 Series is a line of luxury grand tourers..." Go="Buy Now" url="http://localhost:5173/m8" />
     <Cart Image="https://cdn.prod.website-files.com/658041bea130e32d347c0f3e/658041bea130e32d347c1280_test-bmw-x1-sdrive-18d%20(1).webp" Card="X1" Some="The BMW X1 is a subcompact luxury crossover SUV..." Go="Buy Now" url="http://localhost:5173/x1" />
-    <Cart Image="https://www.bowkermotorgroup.co.uk/wp-content/uploads/2025/04/P90585145_highRes_the-new-bmw-ix-xdriv-scaled.jpg" Card="IX" Some="The BMW iX is a fully electric luxury SUV..." Go="Buy Now" url="http://localhost:5173/IX" />
+    <Cart Image="https://www.bowkermotorgroup.co.uk/wp-content/uploads/2025/04/P90585145_highRes_the-new-bmw-ix-xdriv-scaled.jpg" Card="IX" Some="The BMW iX is a fully electric luxury SUV. Most Famous car" Go="Buy Now" url="http://localhost:5173/IX" />
     <Cart Image="https://namastecar.com/wp-content/uploads/2024/09/Luxury-on-Wheels-Indias-Only-BMW-XM-Label-Edition-Sells-for-INR-3.15-Crore.jpeg" Card="XM" Some="The BMW XM is a plug-in hybrid SUV and the first standalone M model since the M1..." Go="Buy Now" url="http://localhost:5173/xm" />
   </div>
 </div>
@@ -168,7 +168,7 @@ function App() {
     <div className="section-container">
   <h1 className="section-title">Mahindra</h1>
   <div className="car-section">
-    <Cart Image="https://media.assettype.com/evoindia/2021-08/1c32d636-7de7-4af3-a631-3a89fa2bbba1/xuv700hero.jpeg" Card="XUV700" Some="The Mahindra XUV700 is a popular SUV..." Go="Buy Now" url="http://localhost:5173/xuv700" />
+    <Cart Image="https://media.assettype.com/evoindia/2021-08/1c32d636-7de7-4af3-a631-3a89fa2bbba1/xuv700hero.jpeg" Card="XUV700" Some="The Mahindra XUV700 is a popular SUV. This new model SUV" Go="Buy Now" url="http://localhost:5173/xuv700" />
     <Cart Image="https://stimg.cardekho.com/images/carexteriorimages/930x620/Mahindra/Thar/10743/1690192572470/front-view-118.jpg" Card="Thar" Some="The Mahindra Thar is a rugged and powerful SUV..." Go="Buy Now" url="http://localhost:5173/thar" />
     <Cart Image="https://media.zigcdn.com/media/content/2023/Mar/cover_63ff11009a034.jpg" Card="Scorpio" Some="The Mahindra Scorpio Classic is a rugged and powerful SUV..." Go="Buy Now" url="http://localhost:5173/scorpio" />
     <Cart Image="https://media.zigcdn.com/media/model/2025/Apr/model-extimg-1115094884_930x620.jpg" Card="Bolero" Some="The Mahindra Bolero is a popular utility vehicle in India..." Go="Buy Now" url="http://localhost:5173/bolero" />
